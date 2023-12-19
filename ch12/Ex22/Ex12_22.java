1079ba11302177e5f7202fb381dac8daa69bd56c
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 01:18:21 +0800

    Add files via upload

---
+++ ch12/Ex22/Ex12_22.java
@@ -0,0 +1,8 @@
+package Ex22;
+
+public class Ex12_22 {
+    public static void main(String[] args) {
+        double result = Math.sin(Math.PI / 6) + Math.cos(Math.PI / 3);
+        System.out.println(result);
+    }
+}
