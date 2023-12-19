8bcf1c164a80dfb22b83ab1a7e0938f96f499526
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 01:27:27 +0800

    Add files via upload

---
+++ ch12/Ex25/Ex12_25.java
@@ -0,0 +1,8 @@
+package Ex25;
+
+public class Ex12_25 {
+    public static void main(String[] args) {
+        String s1 = "Rome was not built in a day.";
+        System.out.println("UpperCase= " + s1.toUpperCase());
+    }
+}
