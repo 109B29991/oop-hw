b1f7257c2a9d4c0e87de74b75c6207f4b8f8ac4a
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 00:23:50 +0800

    Add files via upload

---
+++ ch12/Ex14/Ex12_14.java
@@ -0,0 +1,8 @@
+package Ex14;
+
+public class Ex12_14 {
+    public static void main(String[] args) {
+        String str = "Habit is second nature.";
+        System.out.println(str.substring(16, 22));
+    }
+}
