e61ff2c98c3a5bdf35e5c9cbb09186966028a07c
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 01:17:05 +0800

    Add files via upload

---
+++ ch12/Ex20/Ex12_20.java
@@ -0,0 +1,8 @@
+package Ex20;
+
+public class Ex12_20 {
+    public static void main(String[] args) {
+        double result = Math.atan(1) + Math.acos(0.5);
+        System.out.println(result);
+    }
+}
