7871cb7510c6d91056ec8d636f7422b719806f5d
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 00:50:45 +0800

    Add files via upload

---
+++ ch12/Ex18/Ex12_18.java
@@ -0,0 +1,9 @@
+package Ex18;
+
+public class Ex12_18 {
+    public static void main(String[] args) {
+        String myString = "45874356676541331";
+        long myLong = Long.parseLong(myString);
+        System.out.println(myLong);
+    }
+}
