b78e5e6971cabd6d0fbbba387fa4258d5a419bfe
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 01:22:39 +0800

    Add files via upload

---
+++ ch12/Ex23/Ex12_23.java
@@ -0,0 +1,8 @@
+package Ex23;
+
+public class Ex12_23 {
+    public static void main(String[] args) {
+        String stringValue = Double.toString(67.834);
+        System.out.println(stringValue.length());
+    }
+}
