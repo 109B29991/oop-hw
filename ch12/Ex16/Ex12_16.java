fe3865916bb0b0a8d5a5369b54603a67345b52be
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 00:37:07 +0800

    Add files via upload

---
+++ ch12/Ex16/Ex12_16.java
@@ -0,0 +1,9 @@
+package Ex16;
+
+public class Ex12_16 {
+    public static void main(String[] args) {
+        String str = "262904713";
+        int int1 = Integer.parseInt(str);
+        System.out.println(int1 * 2);
+    }
+}
