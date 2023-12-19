cd23433850b368cc77ff1026922a875716c22b8f
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 00:46:49 +0800

    Add files via upload

---
+++ ch12/Ex17/Ex12_17.java
@@ -0,0 +1,8 @@
+package Ex17;
+
+public class Ex12_17 {
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("Milk");
+        System.out.println(str.reverse());
+    }
+}
