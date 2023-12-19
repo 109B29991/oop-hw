a1c24a6d8342e7445b5cddd6ce65810130da472f
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 00:28:20 +0800

    Add files via upload

---
+++ ch12/Ex15/Ex12_15.java
@@ -0,0 +1,9 @@
+package Ex15;
+
+public class Ex12_15 {
+    public static void main(String[] args) {
+        double num = 123.45;
+        String strl = Double.toString(num);
+        System.out.println("String Length: " + strl.length());
+    }
+}
