686d8ad14c5b169a1208352aa6330f00a634de2c
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-19 11:47:04 +0800

    Add files via upload

---
+++ ch12/Ex02/Max.java
@@ -0,0 +1,17 @@
+package Ex02;
+
+public class Max {
+    private int n1, n2;
+
+    public Max(int a, int b) {
+        n1 = a;
+        n2 = b;
+    }
+
+    public int cmpare() {
+        if (n1 >= n2)
+            return n1;
+        else
+            return n2;
+    }
+}
