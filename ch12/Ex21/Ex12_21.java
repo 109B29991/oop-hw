5fb5555684682534e140e7684a9fbbb1daac27b5
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-20 01:11:11 +0800

    Add files via upload

---
+++ ch12/Ex21/Ex12_21.java
@@ -0,0 +1,8 @@
+package Ex21;
+
+public class Ex12_21 {
+    public static void main(String[] args) {
+        String s1 = "Every Dog Has His Day.";
+        System.out.println("LowerCase= " + s1.toLowerCase());
+    }
+}
