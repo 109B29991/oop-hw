e7dcdb32f306c40c45532144bd5ca3446ea27b1d
cl45k6 <145734650+109B29991@users.noreply.github.com>
2023-12-29 10:54:24 +0800

    Add files via upload

---
+++ ch13/Ex13_02.java
@@ -0,0 +1,13 @@
+public class Ex13_02 {
+    public static void main(String[] args) {
+        int num = 12;
+        int den = 0;
+
+        if (den == 0) {
+            System.out.println("除數為0");
+        } else {
+            int ans = num / den;
+            System.out.println("結果: " + ans);
+        }
+    }
+}
